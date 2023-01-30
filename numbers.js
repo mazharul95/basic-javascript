26f3bacd7d7dd3baffb92fd4e391964c35567f2c
mazharul95 <mipiyas07@gmail.com>
2023-01-30 17:45:06 +0600

    number/terget number print using while loop

---
+++ numbers.js
@@ -0,0 +1,13 @@
+// //
+// var numbers = 0;
+// while (numbers < 10) {
+//   console.log(numbers);
+//   numbers++;
+// }
+
+// Target: display 1 to 20
+var numbers = 1;
+while (numbers <= 20) {
+  console.log(numbers);
+  numbers++;
+}
