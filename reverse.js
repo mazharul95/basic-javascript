66574f4c5717458d87bc3784b457754b9e487f21
mazharul95 <mipiyas07@gmail.com>
2023-01-31 01:25:53 +0600

    while & for loop reverse way

---
+++ reverse.js
@@ -0,0 +1,16 @@
+// var num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+
+// //reverse way
+// var num = 10;
+// while (num >= 1) {
+//   console.log(num);
+//   num--;
+// }
+
+for (var i = 10; i >= 1; i--) {
+  console.log(i);
+}
