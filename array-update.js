0cca9811af8c1b9c8d8fd7072e641d39a38c18f5
mazharul95 <mipiyas07@gmail.com>
2023-01-31 03:21:10 +0600

    problem solved

---
+++ array-update.js
@@ -0,0 +1,25 @@
+// var fruits = ["apple", "banana", "orange"];
+// var bananaIndex = fruits.indexOf("banana");
+// fruits[bananaIndex] = "mango";
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push("watermelon");
+// console.log(fruits);
+
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+
+// var marks = [13, 15, 14, 20, 18];
+// for (var i = 0; i < marks.length; i++) {
+//   if (marks[i] >= 15) {
+//     continue;
+//   }
+//   console.log(marks[i]);
+// }
+
+// var name = "false";
+// console.log(typeof name);
+
+
---
+++ average.js
@@ -0,0 +1,13 @@
+var mathMarks = 75.25;
+var biologyMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.5;
+
+var totalMark =
+  mathMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
+
+console.log(totalMark);
+var avg = totalMark / 5;
+avgTwoDecimal = avg.toFixed(2);
+console.log(avgTwoDecimal);
