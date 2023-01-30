395d68213b2d8e299761df59d587214235ed3082
mazharul95 <mipiyas07@gmail.com>
2023-01-30 23:03:50 +0600

    modified while loop and define for loop

---
+++ forLoop.js
@@ -0,0 +1,25 @@
+//while loop
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//   console.log("Roast, den please !!");
+//   roastGiven++;
+// }
+
+// // for loop
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//   console.log("Roast Den plz !!!");
+// }
+
+// simple version of for loop
+// for (var i = 0; i < 7; i++) {
+//   console.log(i);
+// }
+// i = i + 2;
+// shorthand: i += 2;
+for (var i = 1; i <= 10; i += 2) {
+  console.log(i);
+}
+
+for(var i = 1; i <= 10; i+=2){
+  console.log()
+}
