3f8552447984aca61b6a70f4f9d795bed63bbc30
mazharul95 <mipiyas07@gmail.com>
2023-02-01 16:05:17 +0600

    practice basic function return and modify function parameter

--- parameter.js
+++ parameter.js
@@ -11,9 +11,21 @@
 
 // ShowMessage();
 
-function ShowMessage(firstName, lastName, titleName) {
-  console.log("hello " + firstName + " " + lastName + " " + titleName);
+// function ShowMessage(firstName, lastName, titleName) {
+//   console.log("hello " + firstName + " " + lastName + " " + titleName);
+// }
+// ShowMessage("mazharul", "islam", "Mr");
+// ShowMessage("mi piyash");
+// ShowMessage(28500, 50000);
+
+function add(num1, num2) {
+  console.log("going to add:", num1, num2);
 }
-ShowMessage("mazharul", "islam", "Mr");
-ShowMessage("mi piyash");
-ShowMessage(28500, 50000);
+add(125, 75);
+
+function sum(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  var sum = a + b + c + d + e;
+  console.log(sum);
+}
+sum(100, 220, 30, 40, 50);
