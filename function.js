e9e13401a7276b332eb7dc12251930ba0485e861
mazharul95 <mipiyas07@gmail.com>
2023-02-01 14:17:36 +0600

    practice basic function and function parameter

---
+++ function.js
@@ -0,0 +1,15 @@
+var name = "ariana grande";
+
+function startFan() {
+  console.log("stand up");
+  console.log("walk towards the switch");
+  console.log("pres the switch");
+}
+startFan();
+console.log("walk every morning");
+console.log("i was eaten my breakfast at 10am");
+
+startFan();
+console.log("walk every night");
+console.log("i was eaten my lunch at 1:00am");
+startFan();
---
+++ parameter.js
@@ -0,0 +1,19 @@
+// function bringSingara(money) {
+//   console.log("eito taka disen: ", money);
+//   console.log("ei nen singara");
+// }
+// var taka = 300;
+// bringSingara(taka);
+
+// function ShowMessage() {
+//   console.log("Hello mazharul!");
+// }
+
+// ShowMessage();
+
+function ShowMessage(firstName, lastName, titleName) {
+  console.log("hello " + firstName + " " + lastName + " " + titleName);
+}
+ShowMessage("mazharul", "islam", "Mr");
+ShowMessage("mi piyash");
+ShowMessage(28500, 50000);
