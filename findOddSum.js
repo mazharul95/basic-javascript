fa7599b5da60860ffeef2a9ec049f016b1d7d7d9
mazharul95 <mipiyas07@gmail.com>
2023-02-04 16:20:03 +0600

    Get Odd Numbers of an array and get odd Sum of an array

--- findOddSum.js
+++ findOddSum.js
@@ -5,10 +5,27 @@ function getSum(numbers) {
     let index = i;
     let element = numbers[index];
     sum = sum + element;
-    console.log(index, element, sum);
+    // console.log(index, element, sum);
   }
   return sum;
 }
 
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSum(myNumbers);
+function getOddNumbersOfAnArray(numbers) {
+  const oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const index = i;
+    const element = numbers[index];
+    if (element % 2 === 1) {
+      console.log(index, element);
+      oddNumbers.push(element);
+    }
+  }
+  return oddNumbers;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91, 96, 107, 38];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumberSum = getSum(oddNumbers);
+console.log("Odd number sum", oddNumberSum);
+//getSum(myNumbers);
