f94509ddf2cd2e2fa0f4ef4b902db88584b12c29
mazharul95 <mipiyas07@gmail.com>
2023-02-05 00:43:05 +0600

    factorial of a number using for while loop

---
+++ factorial-while.js
@@ -0,0 +1,12 @@
+function factorial(number) {
+  let num = 1;
+  let result = 1;
+  while (num <= number) {
+    result = result * num;
+    num++;
+  }
+  return result;
+}
+const numbers = 6;
+const fact = factorial(numbers);
+console.log("factorial of:", numbers, fact);
