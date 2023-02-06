8ee79c2cba68a1b12af0ff509c9806472ec9c964
mazharul95 <mipiyas07@gmail.com>
2023-02-06 13:43:42 +0600

    swap variable,swap without temp,destructing

---
+++ swap.js
@@ -0,0 +1,12 @@
+let first = 5;
+let second = 7;
+console.log(first, second);
+
+//approach -1
+// const temp = first;
+// first = second;
+// second = temp;
+
+//approach -2 ---Destructuring
+[first, second] = [second, first];
+console.log(first, second);
