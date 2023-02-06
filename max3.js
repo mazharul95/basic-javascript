3e0f0c8e030910166b4aae6587d0356de381345d
mazharul95 <mipiyas07@gmail.com>
2023-02-06 13:48:56 +0600

    function that will take 3 numbers return the max number using if else if

---
+++ max3.js
@@ -0,0 +1,12 @@
+//problem 2
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if (jim > dela && jim > chinku) {
+  console.log("jim will get the cake");
+} else if (dela > jim && dela > chinku) {
+  console.log("dela will get the cake in this bela");
+} else {
+  console.log("chinku will get the cake");
+}
