758f093a8ad953c26935b1131877095faa683646
mazharul95 <mipiyas07@gmail.com>
2023-02-14 06:08:31 +0600

    Most common ways to set Event Handlers

--- event.js
+++ event.js
@@ -1,7 +1,45 @@
-// target : 1 to 10 all even number
-var number = 0;
-while (number <= 10) {
-  console.log(number);
-  //number++;
-  number = number + 2;
+//option 1: directly Set On the html element
+//<button onclick="console.log(65)">Another button</button>
+
+//option 2: add onclick function
+// <button onclick="makeRed()">Make Red</button>
+
+function makeRed() {
+  document.body.style.backgroundColor = "red";
 }
+
+//option 3:
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+//option 3 another
+const purpleButton = document.getElementById("make-purple");
+purpleButton.onclick = function makePurple() {
+  document.body.style.backgroundColor = "purple";
+};
+
+//option 4
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", makePink);
+
+function makePink() {
+  document.body.style.backgroundColor = "pink";
+}
+
+//option 4 another
+const makeGreenButton = document.getElementById("make-green");
+makeGreenButton.addEventListener("click", function makeGreen() {
+  document.body.style.backgroundColor = "green";
+});
+
+//option 4 final
+//important: we will use some times
+document
+  .getElementById("make-goldenrod")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "goldenrod";
+  });
